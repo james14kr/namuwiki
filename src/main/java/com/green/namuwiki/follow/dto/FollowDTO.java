55839b54a4aa6549a426f7f1e32c56eef0b4f24d
james14kr <hwangms14kr@naver.com>
2026-04-08 14:11:51 +0900

    팔로우 기능 구현 중

---
+++ src/main/java/com/green/namuwiki/follow/dto/FollowDTO.java
@@ -0,0 +1,16 @@
+package com.green.namuwiki.follow.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class FollowDTO {
+
+  private String followerEmail;
+  private String farmerEmail;
+  private String farmerNickname;
+
+}
---
+++ src/main/java/com/green/namuwiki/follow/mapper/FollowMapper.java
@@ -0,0 +1,15 @@
+package com.green.namuwiki.follow.mapper;
+
+import com.green.namuwiki.follow.dto.FollowDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface FollowMapper {
+
+  void follow(FollowDTO dto);
+  void unfollow(FollowDTO dto);
+  List<FollowDTO> getFollowList(String followerEmail);
+
+}
