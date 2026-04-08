471d48af5fbd0ee6dc0cb19f07b8957e12924d0a
rladbwjd0817 <rladbwjd0817@gmail.com>
2026-04-08 17:42:31 +0900

    변수명 변경

--- src/main/java/com/green/namuwiki/member/mapper/DeviceAuthInfoMapper.java
+++ src/main/java/com/green/namuwiki/member/mapper/DeviceAuthInfoMapper.java
@@ -11,4 +11,9 @@ public interface DeviceAuthInfoMapper {
 
   // 인증번호 검증 쿼리 실행
   int selectAuthCode(String authCode);
+
+  // 인증번호 유효성 검사 시 일치여부 판단하는 쿼리 실행
+  int compareFarmerData(DeviceAuthInfoDTO deviceAuthInfoDTO);
+
+
 }
