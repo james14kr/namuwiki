294d3199b25e333f7f6c6acedcb52e291a174319
james14kr <hwangms14kr@naver.com>
2026-04-10 15:26:02 +0900

    feat: 기기 관리 Service, Controller 추가

--- src/main/java/com/green/namuwiki/device/controller/DeviceController.java
+++ src/main/java/com/green/namuwiki/device/controller/DeviceController.java
@@ -1,4 +1,42 @@
 package com.green.namuwiki.device.controller;
 
+import com.green.namuwiki.device.dto.DeviceDTO;
+import com.green.namuwiki.device.service.DeviceService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/device")
 public class DeviceController {
+
+  private final DeviceService deviceService;
+
+  //관리자: 기기 ID 생성
+  @PostMapping("/admin")
+  public ResponseEntity<?> createDevice(@RequestParam String deviceId){
+    deviceService.createDevice(deviceId);
+    return ResponseEntity.ok().build();
+  }
+
+  // 관리자: 전체 기기 목록 조회
+  // GET /device/admin
+  @GetMapping
+  public ResponseEntity<List<DeviceDTO>> getAllDevices(){
+    return ResponseEntity.ok(deviceService.getAllDevices());
+  }
+
+  //농장주: 기기 등록(기기 ID + 농작물 연결)
+  // POST /device/farmer
+  @PostMapping("/register")
+  public ResponseEntity<?> registerDevice(@RequestBody DeviceDTO dto){
+    deviceService.registerDevice(dto);
+    return ResponseEntity.ok().build();
+  }
+
 }
